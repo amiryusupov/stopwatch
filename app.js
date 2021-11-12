df57ae51c72feecb4aa963097b30b7bf39693498
amirdev <amiryusupov.070@gmail.com>
2021-11-12 20:46:48 +0500

    commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let [second,minute,hour] = [0,0,0];
+let timer = document.querySelector('.time');
+let number = document.querySelector('.time-number span');
+let int = null;
+
+function displayTimer(){
+    second+=1;
+    if(second === 59){
+        second = 0;
+        minute++;
+        if(minute === 59){
+            minute = 0;
+            hour++;
+            if(hour === 24){
+                hour = 0;
+                hour++;
+            }
+        }
+    }
+    let h = hour < 10 ? "0" + hour : hour;
+    let m = minute < 10 ? "0" + minute : minute;
+    let s = second < 10 ? "0" + second : second;
+
+    timer.innerHTML = ` ${h} : ${m} : ${s}`;
+}
+
+document.querySelector('#btn-start').addEventListener('click', ()=>{
+    if(int!==null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,1000);
+    number.innerHTML = "..."
+});
+
+document.querySelector('#btn-end').addEventListener('click', ()=>{
+    number.innerHTML = second,minute,hour
+    clearInterval(int);
+    [second,minute,hour] = [0,0,0,0];
+    timer.innerHTML = '00 : 00 : 00';
+});
