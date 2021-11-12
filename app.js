3871c16a268e04b4c60782915373951f6c21419b
amirdev <amiryusupov.070@gmail.com>
2021-11-12 21:18:01 +0500

    commit

--- app.js
+++ app.js
@@ -31,9 +31,20 @@ document.querySelector('#btn-start').addEventListener('click', ()=>{
     int = setInterval(displayTimer,1000);
     number.innerHTML = "..."
 });
+document.querySelector('#btn-pause').addEventListener('click', ()=>{
+    let h = hour < 10 ? "0" + hour : hour;
+    let m = minute < 10 ? "0" + minute : minute;
+    let s = second < 10 ? "0" + second : second;
 
+    number.innerHTML = ` ${h} : ${m} : ${s}`;
+    clearInterval(int);
+});
 document.querySelector('#btn-end').addEventListener('click', ()=>{
-    number.innerHTML = second,minute,hour
+    let h = hour < 10 ? "0" + hour : hour;
+    let m = minute < 10 ? "0" + minute : minute;
+    let s = second < 10 ? "0" + second : second;
+
+    number.innerHTML = ` ${h} : ${m} : ${s}`;
     clearInterval(int);
     [second,minute,hour] = [0,0,0,0];
     timer.innerHTML = '00 : 00 : 00';
